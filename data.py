156ddd1aa19078f9b787a3d80b15fa4dd6ab8947
eunbeejang <eunbee.jang@mail.mcgill.ca>
2019-02-25 00:25:51 -0500

    removed vocab builder for some fields

--- data.py
+++ data.py
@@ -16,14 +16,14 @@ from torchtext.vocab import GloVe, Vectors
 import re
 
 lemmatizer = WordNetLemmatizer()  # is it redundant putting here also, even if I initialize in class?
-
+"""
 def tokenization(self):
     tokenize = lambda x: self.lemmatizer.lemmatize(re.sub(r'<.*?>|[^\w\s]|\d+', '', x)).split()
     # clean_data = re.sub(r'[^\w\s]','', TEXT) # remove punctuation
     # clean_data = clean_data.lower() # convert to lower case
     # clean_data = [lemmatizer.lemmatize(x) for x in (clean_data)]
     return tokenize
-
+"""
 
 class GapDataset(object):  # one seperate object, formal way to declare object
 
@@ -58,12 +58,13 @@ class GapDataset(object):  # one seperate object, formal way to declare object
             ('ID', None),
             ('Text', TEXT),
             ('Pronoun', PRONOUN),
-            ('Pronoun-offset', P_OFFSET),
+            ('Pronoun_off', P_OFFSET),
             ('A', A),
-            ('A-offset', A_OFFSET),
-            ('A-coref', A_COREF),
+            ('A_off', A_OFFSET),
+            ('A_coref', A_COREF),
             ('B', A),
-            ('B-coref', B_COREF),
+            ('B_off', B_OFFSET)
+            ('B_coref', B_COREF),
             ('URL', None)        
         ]
         
@@ -72,32 +73,24 @@ class GapDataset(object):  # one seperate object, formal way to declare object
         test = data.TabularDataset(path=self.test_path, format='tsv', fields=input_fields, skip_header=True)
 
 
-        list_of_A = [x for x in train.A]
-        list_of_B = [x for x in train.B]
-        AB_concat = list_of_A + list_of_B
-        
 
         ##MAP WORDS & FIGURE OUT THE MAX SIZE FOR BUILDING VOCAB
 
         TEXT.build_vocab(train, max_size=30000, vectors=GloVe(name='6B', dim=300))  # Glove Embedding
         PRONOUN.build_vocab(train.PRONOUN)
-        P_OFFSET.build_vocab(train)
-        A_OFFSET.build_vocab(train)
-        B_OFFSET.build_vocab(train)
-        A.build_vocab(train)
-        B.build_vocab(train)
-        NE_LABEL.build_vocab(AB_concat)
-        #A.build_vocab(train.A, train.B)
-        #B.vocab = A.vocab
 
-        A_COREF.build_vocab(train)
-        B_COREF.build_vocab(train)
+        # NE emb
+        list_of_A = [x for x in train.A]
+        list_of_B = [x for x in train.B]
+        AB_concat = list_of_A + list_of_B
+        NE_LABEL.build_vocab(AB_concat)
 
         word_emb = TEXT.vocab.vectors
+        pro_emb = PRONOUN.vocab.vectors
         NE_emb = NE_LABEL.vocab.vectors
         vocab_size = len(TEXT.vocab)
         
-        
+        # if want to use bucket iterator (batching)
         train_data, valid_data, test_data = data.BucketIterator.splits((train, valid, test),
                                                                        batch_size=self.batch_size,
                                                                        repeat=False, shuffle=True)
@@ -105,12 +98,10 @@ class GapDataset(object):  # one seperate object, formal way to declare object
         
         print ("Length of Text Vocabulary: " + str(len(TEXT.vocab)))
         print ("Vector size of Text Vocabulary: ", TEXT.vocab.vectors.size())
-        print ("A Length: " + str(len(A.vocab)))
-        print ("B Length: " + str(len(B.vocab)))
         print ("NE Length: " + str(len(NE_LABEL.vocab)))
         print ("\nSize of train set: {} \nSize of validation set: {} \nSize of test set: {}".format(len(train_data.dataset), len(valid_data.dataset), len(test_data.dataset)))
         
         
         
-        return TEXT, NE_LABEL, word_emb, NE_emb, train_data, valid_data, test_data
+        return TEXT, PRONOUN, NE_LABEL, word_emb, pro_emb, NE_emb, train_data, valid_data, test_data, train, valid, test
 
