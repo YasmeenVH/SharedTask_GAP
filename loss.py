2e10e5499f11f83908090cbdebdece46693b31f5
k1c <cpelletier@klipfolio.com>
2019-02-25 10:05:52 -0500

    re-adding SVM loss function

--- loss.py
+++ loss.py
@@ -25,3 +25,30 @@ class coref_loss(nn.Module):
         """
         return pass
 
+class multiClassHingeLoss(nn.Module):
+    def __init__(self, p=1, margin=1, weight=None, size_average=True):
+        super(multiClassHingeLoss, self).__init__()
+        self.p=p
+        self.margin=margin
+        self.weight=weight#weight for each class, size=n_class
+        self.size_average=size_average
+
+     def forward(self, output, y):#output: batchsize*n_class
+        output_y=output[torch.arange(0,y.size()[0]).long(),y.data].view(-1,1) #view for transpose
+        #margin - output[y] + output[i]
+        loss=output-output_y+self.margin #contains i=y
+        #remove i=y items
+        loss[torch.arange(0,y.size()[0]).long(),y.data]=0
+        #max(0,_)
+        loss[loss<0]=0.
+         #power
+        if(self.p!=1):
+            loss=torch.pow(loss,self.p)
+        #add weight
+        if(self.weight is not None):
+            loss=loss*self.weight
+        #sum up
+        loss=torch.sum(loss)
+        if(self.size_average):
+            loss/=output.size()[0]
+        return loss
