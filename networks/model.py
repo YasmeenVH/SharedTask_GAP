01ce1e48e21c4fa31d8b140715d2a8ab39c7d376
YasmeenVH <yasmeen.hitti@mail.mcgill.ca>
2019-02-26 10:36:31 -0500

    changed input output initialization

--- networks/model.py
+++ networks/model.py
@@ -3,9 +3,11 @@ import torch
 
 class LogisticRegression(torch.nn.Module):
 
-    def __init__(self):
+    def __init__(self, input_size, output_size):
         super(LogisticRegression, self).__init__()
-        self.h1 = torch.nn.Linear(7,2) #we have 7 inputs and predicted true or false for 2 antecedents
+        self.input_size = input_size
+        self.output_size = output_size
+        self.h1 = torch.nn.Linear(self.input_size, self.output_size) #we have 7 inputs and predicted true or false for 2 antecedents
         #self.h2 = torch.nn.Linear() we will start with one
 
     def __forward__(self, x):
