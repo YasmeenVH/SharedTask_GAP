252f67b9cf5e0b08d0a08f0a2a4b8dc91b2079e9
eunbeejang <eunbee.jang@mail.mcgill.ca>
2019-03-09 01:24:37 -0500

    scripts connected

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import torch
+import torch.optim as optim
 from torch.nn import functional
 import sys
 sys.path.insert(0, './networks')
@@ -11,10 +12,13 @@ from networks.feedforward import feedforward_nn
 
 
 #Data path
-
+"""
 train_path = 'C:/Users/Y/Documents/MILA/SharedTask_GAP/Data/gap-development.tsv'
 test_path = 'C:/Users/Y/Documents/MILA/SharedTask_GAP/Data/gap-test.tsv'
 valid_path = 'C:/Users/Y/Documents/MILA/SharedTask_GAP/Data/gap-validation.tsv'
+
+
+
 
 #Load data
 dataset = GapDataset(train_path, test_path, valid_path)
@@ -23,22 +27,21 @@ output_size = 2
 logistic = LogisticRegression(input_size, output_size)
 ff = feedforward_nn(input_size, output_size)
 svm = SVM(input_size, output_size)
+"""
+
 
 class model_train(object):
     num_of_train = 0
     num_of_eval = 0
 
-    def __init__(self, model, train_data, valid_data, test_data, epoch_size, optimizer, max_grad_norm):
+    def __init__(self, model, train_data, valid_data, test_data, epoch_size):
         self.model = model
         self.train_data = train_data
         self.valid_data = valid_data
         self.test_data = test_data
         self.epoch_size = epoch_size
-        self.optimizer = optimizer
-        self.max_grad_norm = max_grad_norm
 
-
-    def train(self):
+    def train(self, optimizer, max_grad_norm):
 
         model_train.num_of_train += 1 # increments training times
 
@@ -46,7 +49,8 @@ class model_train(object):
 
         total_loss = []
         total_acc = []
-
+        print("%%%%% ENTERING HERE")
+        # YASMEEN, YOU CAN CONTINUE DEBUGGING FROM HERE
         for epoch in range(self.epoch_size):
             epoch_loss = []
             epoch_acc = []
@@ -57,7 +61,8 @@ class model_train(object):
 
                 batch_size = len(batch.text[0])
                 y_pred = self.model(batch.text[0], batch_size)
-                y_target = self.B
+                print(y_pred)
+                y_target = self.B # what are you calling? we don't have self.B
                 loss = self.bce_loss( y_pred, y_target) #size_average=False       not too sure why this was here before
                 correct = ((torch.max(pred, 1)[1] == y_target)).sum().numpy()
                 acc = correct/pred.shape[0]
@@ -106,7 +111,37 @@ class model_train(object):
         return avg_total_loss, total_loss, total_acc
 
 
+
+
+
+# THIS WILL BE CALLED WHEN RUNNING
+def main():
+
+    input_size = 7
+    output_size = 2
+    logistic = LogisticRegression(input_size, output_size)
+    train_path = './data/gap-development.tsv'
+    valid_path = './data/gap-validation.tsv'
+    test_path = './data/gap-test.tsv'
+
+    dataset = GapDataset(train_path, valid_path,test_path)
+    TEXT, PRONOUN, NE_LABEL, word_emb, pro_emb, NE_emb, train_data, valid_data, test_data, _, _, _ = dataset.loader()
+    logistic_reg = model_train(logistic, train_data, valid_data, test_data, 20)
+    optimizer = optim.SGD(logistic.parameters(), lr = 0.01, momentum=0.9)
+    max_grad_norm = 5
+    totla_loss, total_acc = logistic_reg.train(optimizer, max_grad_norm)
+
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
-    dataset = GapDataset(train_path, test_path, valid_path)
-    data_loader = dataset.loader()
-    first_train = model_train(logistic, train_data, valid_data, test_data, epoch_size, optimizer, max_grad_norm)
+    main()
+
+
+
