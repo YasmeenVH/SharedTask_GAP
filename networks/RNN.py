b05137f4e5f430d529729dca376d63148ab7fce7
YasmeenVH <yasmeen.hitti@mail.mcgill.ca>
2019-04-15 14:46:57 -0400

    try without transpose

--- networks/RNN.py
+++ networks/RNN.py
@@ -15,10 +15,26 @@ class RNNLinear(nn.Module):
 
     def forward(self, x):
         print("ENTERING HERE")
-        x = x.transpose(0, 1)  # Input needs to be of dimension (seq_len, batch_size, input_size)
+        #x = x.transpose(0, 1)  # Input needs to be of dimension (seq_len, batch_size, input_size)
         output, hidden_T = self.rnn(x)
         pred = self.linear(hidden_T[-1])
 
         return pred
+
+#def main():
+
+#    run = RNNLinear.forward()
+#    test_path = './data/gap-test.tsv'
+#    dataloader = GapDataset(train_path, valid_path, test_path)
+#    x_train, x_train_pad, y_train = dataloader.load('train', 30)
+
+
+ #   print("DATA LOADED.")
+
+
+
+
+#if __name__ == '__main__':
+    main()
 
 
